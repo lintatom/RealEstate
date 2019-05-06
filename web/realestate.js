79bdd3234c8a7fe4f83b4ba8d1c4dc24976f62f1
USER-4W55N52 <USER-4W55N52@10.1.10.124>
2019-05-06 11:25:02 -0500

    commit 5/6/19

--- web/realestate.js
+++ web/realestate.js
@@ -3,11 +3,5 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var model=document.getElementById("container_mainsignin");
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+
 
