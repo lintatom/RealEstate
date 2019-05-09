8083b420c3c9eb396eabe87d7b5fd2e5b8b46f7c
lintatom <34221141+lintatom@users.noreply.github.com>
2019-05-09 11:28:17 -0500

    added  buyer /seller pages

--- web/realestate.js
+++ web/realestate.js
@@ -1,7 +1,67 @@
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+// Get the modal
+var modal = document.getElementById('container_mainsignin');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+/*----hide the element in signIn Page-----*/
+$(document).ready(function(){
+  $(".signin_link").click(function(){
+    $(".needstoregister-links").hide();
+  });
+  $(".alreadyregistered_link").click(function(){
+    $(".txtname_signin").hide();
+    $(".txtphone_signin").hide();
+    $(".alreadyregistered_link").hide();
+    $(".needstoregister-links").show();
+   // $(".btnregister_signin").text('Login');
+    $(".btnregister_signin").text("Login");
+    
+  });
+  $(".needstoregister-links").click(function(){
+      $(".txtname_signin").show();
+    $(".txtphone_signin").show();
+    $(".alreadyregistered_link").show();
+    $(".needstoregister-links").hide();
+     $(".btnregister_signin").text("Register");
+  });
+  
+});
+/*end SignIn*/
+
+/*dropdown search*/
+  $(".dropdown-menu li a").click(function(){
+                var selText = $(this).text();
+                $(this).parents(".dropdown").find(".dropdown-toggle").html(selText+' <span class="caret"></span>');
+              });
+    
+/*end Search dropdown*/
+
+/*slider*/
+ $( function() {
+     var outputspan=$('#spanOutput');
+   $('#slider').slider({
+   range:true,
+        min:0,
+        max:1000000,
+        values:[70000,280000],
+        slide:function(event,ui){
+            outputspan.html(ui.values[0]+' - '+ ui.values[1]);
+            
+           
+         }
+   });
+  } );
+  /* end Slider*/
 
--- web/scripts/pages.js
+++ web/scripts/pages.js
@@ -11,6 +11,46 @@ function loadPage(page){
                             .then(html => fillContent(html, 'agent-detail'));
                     });
           break;
+          case 'buyers':
+           fetch('./buyers.html')
+                  .then(res => res.text())
+                  .then(html => fillContent(html, 'services'))
+                  .then(function() {
+                      fetch('./profile.html')
+                            .then(res => res.text())
+                            .then(html => fillContent(html, 'buyer_img'));
+                        fetch('./contact.html')
+                            .then(res => res.text())
+                            .then(html => fillContent(html, 'buyer_contact'));
+                    });
+          break;
+          case 'sellers':
+           fetch('./sellers.html')
+                  .then(res => res.text())
+                  .then(html => fillContent(html, 'services'))
+                  .then(function() {
+                      fetch('./profile.html')
+                            .then(res => res.text())
+                            .then(html => fillContent(html, 'buyer_img'));
+                        fetch('./contact.html')
+                            .then(res => res.text())
+                            .then(html => fillContent(html, 'buyer_contact'));
+                    });
+          break;
+          
+          case 'about':
+           fetch('./about.html')
+                  .then(res => res.text())
+                  .then(html => fillContent(html, 'services'))
+                  .then(function() {
+                      fetch('./profile.html')
+                            .then(res => res.text())
+                            .then(html => fillContent(html, 'about_agentimg'));
+                        fetch('./contact.html')
+                            .then(res => res.text())
+                            .then(html => fillContent(html, 'about_contact'));
+                    });
+          break;
           
       default:
           fetch('./home.html')
